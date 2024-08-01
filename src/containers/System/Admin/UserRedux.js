4200c030d6a66305f40b6f463ac238a6716de903
ducduy1102 <ducduy1110u@gmail.com>
2024-08-01 23:58:32 +0700

    Add preview image

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -2,17 +2,36 @@ import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import { LANGUAGES } from "../../../utils";
-import { fetchGenderStart } from "../../../store/actions";
+import {
+  fetchGenderStart,
+  fetchPositionStart,
+  fetchRoleStart,
+} from "../../../store/actions";
+import "../UserManage.scss";
+import "./UserRedux.scss";
+import "react-image-lightbox/style.css";
+import Lightbox from "react-image-lightbox";
 
 class UserRedux extends Component {
   constructor(props) {
     super(props);
     this.state = {
       genderArr: [],
+      positionArr: [],
+      roleArr: [],
+      previewImagURL: "",
+      isOpen: false,
     };
   }
   async componentDidMount() {
     this.props.getGenderStart();
+    this.props.getPositionStart();
+    this.props.getRoleStart();
+    // this.initLightboxJS(
+    //   "Insert your License Key here",
+    //   "Insert plan type here"
+    // );
+
     // try {
     //   let res = await getAllCodeService("GENDER");
     //   if (res && res.errCode === 0) {
@@ -27,7 +46,7 @@ class UserRedux extends Component {
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    // render => DidUpdate
+    // component re-render => DidUpdate
     // so sánh hiện tại (this) và quá khứ (prev)
     // [3] vs []
     if (prevProps.genderRedux !== this.props.genderRedux) {
@@ -35,152 +54,225 @@ class UserRedux extends Component {
         genderArr: this.props.genderRedux,
       });
     }
+    if (prevProps.roleRedux !== this.props.roleRedux) {
+      this.setState({
+        roleArr: this.props.roleRedux,
+      });
+    }
+    if (prevProps.positionRedux !== this.props.positionRedux) {
+      this.setState({
+        positionArr: this.props.positionRedux,
+      });
+    }
   }
 
+  handleOnChangeImage = (e) => {
+    let file = e.target.files[0];
+    // console.log("objecturrl", objectUrl);
+    if (file) {
+      let objectUrl = URL.createObjectURL(file);
+      this.setState({
+        previewImagURL: objectUrl,
+      });
+    }
+  };
+  openPreviewImage = () => {
+    if (!this.state.previewImagURL) return;
+    this.setState({
+      isOpen: true,
+    });
+  };
   render() {
     // console.log("check state", this.state);
-    let genders = this.state.genderArr;
-    let language = this.props.language;
-    console.log("check props from redux", this.props.genderRedux);
+    const { language, isLoading } = this.props;
+    const { genderArr, positionArr, roleArr } = this.state;
+    // console.log("check props from redux", this.props);
+    // console.log("check state from redux", this.state);
     return (
       <div className="container user-redux-container">
         <div className="title">User Manage Redux</div>
-        <div className="user-redux-body">
-          <div className="container">
-            <div className="row">
-              <form className="row g-3">
-                <div className="my-3 col-12">
-                  <FormattedMessage id="manage-user.add" />
-                </div>
-                <div className="col-md-3">
-                  <label htmlFor="email" className="form-label">
-                    <FormattedMessage id="manage-user.email" />
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    name="email"
-                    placeholder="Enter your email..."
-                  />
-                </div>
-                <div className="col-md-3">
-                  <label htmlFor="password" className="form-label">
-                    <FormattedMessage id="manage-user.password" />
-                  </label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="password"
-                    name="password"
-                    placeholder="Enter your password..."
-                  />
-                </div>
-                <div className="col-md-3">
-                  <label htmlFor="firstName" className="form-label">
-                    <FormattedMessage id="manage-user.first-name" />
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="firstName"
-                    name="firstName"
-                    placeholder="Enter your first name..."
-                  />
-                </div>
-                <div className="col-md-3">
-                  <label htmlFor="lastName" className="form-label">
-                    <FormattedMessage id="manage-user.last-name" />
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="lastName"
-                    name="lastName"
-                    placeholder="Enter your last name..."
-                  />
-                </div>
-                <div className="col-md-3">
-                  <label htmlFor="phoneNumber" className="form-label">
-                    <FormattedMessage id="manage-user.phone-number" />
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="phoneNumber"
-                    name="phoneNumber"
-                    placeholder="Enter your phone number..."
-                  />
-                </div>
-                <div className="col-md-9">
-                  <label htmlFor="address" className="form-label">
-                    <FormattedMessage id="manage-user.address" />
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="address"
-                    name="address"
-                    placeholder="Enter your address..."
-                  />
-                </div>
-                <div className="col-md-3">
-                  <label htmlFor="gender" className="form-label">
-                    <FormattedMessage id="manage-user.gender" />
-                  </label>
-                  <select id="gender" className="form-select" name="gender">
-                    {genders &&
-                      genders.length > 0 &&
-                      genders.map((item, index) => {
-                        return (
-                          <option key={`gender-${index}`}>
-                            {language === LANGUAGES.VI
-                              ? item.valueVi
-                              : item.valueEn}
-                          </option>
-                        );
-                      })}
-                  </select>
-                </div>
-                <div className="col-md-3">
-                  <label htmlFor="position" className="form-label">
-                    <FormattedMessage id="manage-user.position" />
-                  </label>
-                  <select id="position" className="form-select" name="position">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                  </select>
-                </div>
-                <div className="col-md-3">
-                  <label htmlFor="roleId" className="form-label">
-                    <FormattedMessage id="manage-user.role" />
-                  </label>
-                  <select id="roleId" className="form-select" name="roleId">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                  </select>
-                </div>
-                <div className="col-md-3">
-                  <label htmlFor="avatar" className="form-label">
-                    <FormattedMessage id="manage-user.avatar" />
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="avatar"
-                    name="avatar"
-                    placeholder="Enter your avatar..."
-                  />
-                </div>
-                <div className="col-12">
-                  <button type="submit" className="btn btn-primary">
-                    <FormattedMessage id="manage-user.save" />
-                  </button>
-                </div>
-              </form>
+        {isLoading === true ? (
+          <div className="loader-container">
+            <div className="loader"></div>
+          </div>
+        ) : (
+          <div className="user-redux-body">
+            <div className="container">
+              <div className="row">
+                <form className="row g-3">
+                  <div className="my-3 col-12">
+                    <FormattedMessage id="manage-user.add" />
+                  </div>
+                  <div className="col-md-3">
+                    <label htmlFor="email" className="form-label">
+                      <FormattedMessage id="manage-user.email" />
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      name="email"
+                      placeholder="Enter your email..."
+                    />
+                  </div>
+                  <div className="col-md-3">
+                    <label htmlFor="password" className="form-label">
+                      <FormattedMessage id="manage-user.password" />
+                    </label>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      name="password"
+                      placeholder="Enter your password..."
+                    />
+                  </div>
+                  <div className="col-md-3">
+                    <label htmlFor="firstName" className="form-label">
+                      <FormattedMessage id="manage-user.first-name" />
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="firstName"
+                      name="firstName"
+                      placeholder="Enter your first name..."
+                    />
+                  </div>
+                  <div className="col-md-3">
+                    <label htmlFor="lastName" className="form-label">
+                      <FormattedMessage id="manage-user.last-name" />
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="lastName"
+                      name="lastName"
+                      placeholder="Enter your last name..."
+                    />
+                  </div>
+                  <div className="col-md-3">
+                    <label htmlFor="phoneNumber" className="form-label">
+                      <FormattedMessage id="manage-user.phone-number" />
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="phoneNumber"
+                      name="phoneNumber"
+                      placeholder="Enter your phone number..."
+                    />
+                  </div>
+                  <div className="col-md-9">
+                    <label htmlFor="address" className="form-label">
+                      <FormattedMessage id="manage-user.address" />
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="address"
+                      name="address"
+                      placeholder="Enter your address..."
+                    />
+                  </div>
+                  <div className="col-md-3">
+                    <label htmlFor="gender" className="form-label">
+                      <FormattedMessage id="manage-user.gender" />
+                    </label>
+                    <select id="gender" className="form-select" name="gender">
+                      {genderArr &&
+                        genderArr.length > 0 &&
+                        genderArr.map((item, index) => {
+                          return (
+                            <option key={`gender-${index}`}>
+                              {language === LANGUAGES.VI
+                                ? item.valueVi
+                                : item.valueEn}
+                            </option>
+                          );
+                        })}
+                    </select>
+                  </div>
+                  <div className="col-md-3">
+                    <label htmlFor="position" className="form-label">
+                      <FormattedMessage id="manage-user.position" />
+                    </label>
+                    <select
+                      id="position"
+                      className="form-select"
+                      name="position"
+                    >
+                      {positionArr &&
+                        positionArr.length > 0 &&
+                        positionArr.map((item, index) => {
+                          return (
+                            <option key={`position-${index}`}>
+                              {language === LANGUAGES.VI
+                                ? item.valueVi
+                                : item.valueEn}
+                            </option>
+                          );
+                        })}
+                    </select>
+                  </div>
+                  <div className="col-md-3">
+                    <label htmlFor="roleId" className="form-label">
+                      <FormattedMessage id="manage-user.role" />
+                    </label>
+                    <select id="roleId" className="form-select" name="roleId">
+                      {roleArr &&
+                        roleArr.length > 0 &&
+                        roleArr.map((item, index) => {
+                          return (
+                            <option key={`role-${index}`}>
+                              {language === LANGUAGES.VI
+                                ? item.valueVi
+                                : item.valueEn}
+                            </option>
+                          );
+                        })}
+                    </select>
+                  </div>
+                  <div className="col-md-3">
+                    <label htmlFor="avatar" className="form-label">
+                      <FormattedMessage id="manage-user.avatar" />
+                    </label>
+                    <div className="preview-img-container">
+                      <input
+                        type="file"
+                        name=""
+                        id="preview-img"
+                        onChange={(e) => this.handleOnChangeImage(e)}
+                        hidden
+                      />
+                      <label className="label-upload" htmlFor="preview-img">
+                        Tải ảnh <i className="fas fa-cloud-upload-alt"></i>
+                      </label>
+                      <div
+                        className="preview-img"
+                        style={{
+                          backgroundImage: `url(${this.state.previewImagURL})`,
+                        }}
+                        onClick={() => this.openPreviewImage()}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="col-12">
+                    <button type="submit" className="btn btn-primary">
+                      <FormattedMessage id="manage-user.save" />
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
+        )}
+        {this.state.isOpen === true && (
+          <Lightbox
+            mainSrc={this.state.previewImagURL}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+          />
+        )}
       </div>
     );
   }
@@ -189,6 +281,9 @@ class UserRedux extends Component {
 const mapStateToProps = (state) => {
   return {
     genderRedux: state.admin.genders,
+    positionRedux: state.admin.positions,
+    roleRedux: state.admin.roles,
+    isLoading: state.admin.isLoading,
     language: state.app.language,
   };
 };
@@ -196,6 +291,9 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     getGenderStart: () => dispatch(fetchGenderStart()),
+    getPositionStart: () => dispatch(fetchPositionStart()),
+    getRoleStart: () => dispatch(fetchRoleStart()),
+
     // processLogout: () => dispatch(actions.processLogout()),
     // changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language)),
   };
