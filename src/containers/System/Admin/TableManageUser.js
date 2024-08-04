b4d657d262e67eaa33b8881297a8fa28461d5c6c
ducduy1102 <ducduy1110u@gmail.com>
2024-08-05 00:51:11 +0700

    Add markdown-editor

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -4,7 +4,21 @@ import { connect } from "react-redux";
 import "./TableManageUser.scss";
 import { deleteUser, fetchAllUserStart } from "../../../store/actions";
 import { LANGUAGES } from "../../../utils";
-// import { emitter } from "../../utils/emitter";
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+// import style manually
+import "react-markdown-editor-lite/lib/index.css";
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+  console.log("handleEditorChange", html, text);
+}
 
 class TableManageUser extends Component {
   constructor(props) {
@@ -142,6 +156,11 @@ class TableManageUser extends Component {
                   )}
                 </tbody>
               </table>
+              <MdEditor
+                style={{ height: "500px" }}
+                renderHTML={(text) => mdParser.render(text)}
+                onChange={handleEditorChange}
+              />
             </div>
           </div>
         </div>
